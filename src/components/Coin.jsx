147daac65813f5cdb2ce6cb03524d838adc498de
Chris Mbah <mbahchris46@gmail.com>
2023-08-24 23:36:02 +0100

    Added path to coin info with use of coin name

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Coin.css";
 
 export default function Coin({ coin, id, filteredList }) {
@@ -15,32 +16,35 @@ export default function Coin({ coin, id, filteredList }) {
   } = coin;
   return (
     <>
-      <div className="coins">
-        <div className="coin-info">
-          <h4>{num}</h4>
-          <div className="coin-img">
-            <img src={image} alt={name} />
+      <Link to={`/info/${name.toLowerCase()}`}>
+        <div className="coins">
+          <div className="coin-info">
+            <h4>{num}</h4>
+            <div className="coin-img">
+              <img src={image} alt={name} />
+            </div>
+            <div className="coin-name">
+              <h1>{name}</h1>
+            </div>
+            <div className="coin-symbol">
+              <span>{symbol}</span>
+            </div>
           </div>
-          <div className="coin-name">
-            <h1>{name}</h1>
-          </div>
-          <div className="coin-symbol">
-            <span>{symbol}</span>
+          <div className="coin-details">
+            <div className="det price">
+              <span>${current_price}</span>
+            </div>
+            <div className="det price-change">
+              {price_change_percentage_24h}
+            </div>
+            <div className="det market-cap">${market_cap}</div>
+            <div className="det market-cap-change">
+              {market_cap_change_percentage_24h}
+            </div>
+            <div className="vol">${total_volume}</div>
           </div>
         </div>
-        <div className="coin-details">
-          <div className="det price">
-            <span>${current_price}</span>
-          </div>
-          <div className="det price-change">{price_change_percentage_24h}</div>
-          <div className="det market-cap">${market_cap}</div>
-          <div className="det market-cap-change">
-            {market_cap_change_percentage_24h}
-          </div>
-          <div className="vol">${total_volume}</div>
-        </div>
-      </div>
-      {filteredList === [] && <div>Oops Coin Not Available</div>}
+      </Link>
     </>
   );
 }
