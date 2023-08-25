cb2587f4033472a86a5b6428f46cbb96cc3c4eaa
Chris Mbah <mbahchris46@gmail.com>
2023-08-25 15:26:48 +0100

    Coin Info api implemented

--- src/components/CoinInfo.jsx
+++ src/components/CoinInfo.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Axios from "axios";
 import { useParams } from "react-router-dom";
+import { useContext } from "react";
+import { CoinApp } from "../App";
 
 export default function CoinInfo() {
-  const { coin } = useParams();
+  const { coinId } = useParams();
+  const { coinInfo, setCoinInfo } = useContext(CoinApp);
 
-  return <div>CoinInfo of {coin}</div>;
+  async function getCoinInfo(coinID) {
+    try {
+      const info = await Axios.get(
+        `https://api.coingecko.com/api/v3/coins/${coinID}`
+      );
+      setCoinInfo(info.data);
+      console.log(coinInfo);
+      console.log(coinId);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    getCoinInfo(coinId);
+  }, [coinId]);
+
+  return (
+    <>
+      Coin Information of {coinId}
+      <div>{coinInfo.name}</div>
+      <div>{coinInfo.symbol}</div>
+    </>
+  );
 }
