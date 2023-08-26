4f810337884f2f9039eb933c69b0d86e498abf94
Chris Mbah <mbahchris46@gmail.com>
2023-08-26 14:47:13 +0100

    Added home page lik to logo for easy routing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import { useContext } from "react";
 import { CoinApp } from "../App";
 import logo from "../assets/logo.svg";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   const { selectedCurrency, handleCurrencyChange } = useContext(CoinApp);
   return (
     <div className="header">
-      <div className="header-title">
-        <h2>CoinView</h2>
-        <img src={logo} alt="logo" />
-      </div>
-
+      <Link to="/">
+        <div className="header-title">
+          <h2>CoinView</h2>
+          <img src={logo} alt="logo" />
+        </div>
+      </Link>
       <div className="curr">
         <select value={selectedCurrency} onChange={handleCurrencyChange}>
           <option value="usd">USD</option>
